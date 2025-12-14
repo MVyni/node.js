0893ed8be0aaffeea202353fd90e7c54cbe9ab2f
Marcus Vynicius <viniciusfla1999@gmail.com>
2025-12-14 20:19:39 +0000

    created prisma check-ins repository

--- api-solid/src/repositories/prisma/prisma-users-repository.ts
+++ api-solid/src/repositories/prisma/prisma-users-repository.ts
@@ -1,11 +1,16 @@
-import { Prisma, type User } from '@/generated/prisma/client.js'
+import { Prisma } from '@/generated/prisma/client.js'
 import { prisma } from '@/lib/prisma.js'
 import type { UsersRepository } from '../users-repository.js'
 
 export class PrismaUserRepository implements UsersRepository{
   
-  findById(id: string): Promise<User | null> {
-    throw new Error('Method not implemented.')
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      }
+    })
+    return user
   }
 
   async findByEmail(email: string) {
